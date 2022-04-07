2b9c3da2fc946b734e0d19719799ea859088331d
Douglas Chen <dougpuob@gmail.com>
2022-04-07 09:38:46 +0800

    Update run-puppet-server.ps1

--- script/run-puppet-server.ps1
+++ script/run-puppet-server.ps1
@@ -2,28 +2,33 @@ $SCRIPT_PATH=$MyInvocation.MyCommand.Definition
 $TASK_NAME="launch-qemu-tasker"
 $QEMU_TASKER_DIR=(Join-Path $ENV:HOMEDRIVE $ENV:HOMEPATH qemu-takser.git)
 
-if (-Not (Get-ScheduledTask | Where-Object {$_.TaskName -like $TASK_NAME})) {
-  $QEMU_TASKER_PY=(Join-Path $QEMU_TASKER_DIR src qemu-takser.py)
+if (-Not (Get-ScheduledTask | Where-Object {$_.TaskName -like $TASK_NAME}))
+{
+  # ----------------------------------------------------------------------------
+  # Register a scheduled task
+  # ----------------------------------------------------------------------------  
   $Trigger= New-ScheduledTaskTrigger -AtStartup
   $User= "NT AUTHORITY\SYSTEM"
-  $Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument ("$QEMU_TASKER_PY {0}" -f "puppet")
-  Register-ScheduledTask -TaskName $TASK_NAME -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force # Specify the name of the task
-} else {
-
+  $Action= New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument $SCRIPT_PATH
+  Register-ScheduledTask -TaskName $TASK_NAME -Trigger $Trigger -User $User -Action $Action -RunLevel Highest –Force
+}
+else 
+{
+  # ----------------------------------------------------------------------------
+  # Prepare and execute qemu-tasker.py as a puppet server
+  # ----------------------------------------------------------------------------
   if (-Not (Test-Path $QEMU_TASKER_DIR)) {
-	mkdir $QEMU_TASKER_DIR
+    mkdir $QEMU_TASKER_DIR
   }
   Push-Location $QEMU_TASKER_DIR
-
 
   # Clone the qemu-tasker project
   git clone https://github.com/dougpuob/qemu-tasker.git .
   git checkout feature-add-puppet-command
 
-
   # Become a puppet server
-  python ./src/qemu-tasker.py puppet
-
+  $QEMU_TASKER_PY = (Join-Path $QEMU_TASKER_DIR src qemu-takser.py)
+  python $QEMU_TASKER_PY puppet
 
   Pop-Location
 }
