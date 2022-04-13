26d77d31e6326ca32c9e9ec8c064596df13f0c0f
Douglas Chen <dougpuob@gmail.com>
2022-04-13 14:32:41 +0800

    WIP

--- src/module/ftpclient.py
+++ src/module/ftpclient.py
@@ -151,14 +151,14 @@ class ftpclient():
             if cmdret.errcode != 0:
                 raise "File not found !!!"
 
-            #
-            # Try to create the directory then change directory
-            #
-            pwd = self.ftp.pwd()
-            cmdret_mkdir = self.mkdir(dir_to_save)
-            cmdret.info_lines.extend(cmdret_mkdir.info_lines)
-            if cmdret_mkdir.errcode != 0:
-                cmdret.error_lines.extend(cmdret_mkdir.error_lines)
+            # #
+            # # Try to create the directory then change directory
+            # #
+            # pwd = self.ftp.pwd()
+            # cmdret_mkdir = self.mkdir(dir_to_save)
+            # cmdret.info_lines.extend(cmdret_mkdir.info_lines)
+            # if cmdret_mkdir.errcode != 0:
+            #     cmdret.error_lines.extend(cmdret_mkdir.error_lines)
 
             #
             # Upload files
